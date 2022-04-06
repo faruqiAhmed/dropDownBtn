c872aef22346243e9d9047144091a8d289169db2
Faruq <faruqdeveloper@gmail.com>
2022-04-06 21:08:49 +0600

    testpupop

--- dropDownBtn/ViewController.swift
+++ dropDownBtn/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+    @IBOutlet weak var Setting: UIButton!
+    
+    @IBOutlet weak var delete: UIButton!
+    
+    @IBOutlet var settingView: UIView!
+    
+    @IBOutlet weak var tbl: UITableView!
+    
+    @IBOutlet weak var tobtm: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        settingView.layer.cornerRadius = 12
+      // settingView.layer.frame = CGRect(x: 0, y: 0, width: 1, height: 3)
     }
 
+    @IBAction func pupopButton(_ sender: UIButton) {
+        
+        
+        
+        self.view.addSubview(settingView)
+       self.settingView.center.x =  self.view.center.x
+        
+        self.settingView.center.y = self.view.center.y - (self.view.frame.height / 7.0)
+        //settingView.layer.frame = CGRect(x: 0, y: 0, width: 1, height: 3)
+        
+        
+        
+    }
+    
+    @IBAction func deleteb(_ sender: UIButton) {
+        
+        UIView.animate (withDuration: 0.4, animations: {
+        
+       
+        })
+        { (status) in
+        
+        self.settingView.removeFromSuperview()
+        }
 
+    }
+    
+   
 }
 
